5a707f4b96808b4f542303437ff96155542471c4
dasurahul <dasurahul598@gmail.com>
2021-06-07 12:13:02 +0530

    Added Pagination

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -4,20 +4,23 @@ import Photo from "./Photo";
 import classes from "./Photos.module.css";
 
 const Photos = (props) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
   const [photos, setPhotos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    const fetchPhotos = async () => {
+    const fetchPhotos = async (page = 1) => {
       setIsLoading(true);
       if (props.searchValue) {
         const response = await fetch(
-          `https://api.unsplash.com/search/photos/?client_id=8zFIPebzf0QHzrvmkTjBEHGNF-gwGiYo4WTQp6huP2M&&query=${props.searchValue}`
+          `https://api.unsplash.com/search/photos/?client_id=8zFIPebzf0QHzrvmkTjBEHGNF-gwGiYo4WTQp6huP2M&&query=${props.searchValue}&&page=${currentPage}`
         );
         const data = await response.json();
         setPhotos(data.results);
+        setTotalPages(data.total_pages);
       } else {
         const response = await fetch(
-          `https://api.unsplash.com/photos/?client_id=8zFIPebzf0QHzrvmkTjBEHGNF-gwGiYo4WTQp6huP2M`
+          `https://api.unsplash.com/photos/?client_id=8zFIPebzf0QHzrvmkTjBEHGNF-gwGiYo4WTQp6huP2M&&per_page=20`
         );
         const data = await response.json();
         setPhotos(data);
@@ -25,7 +28,14 @@ const Photos = (props) => {
       setIsLoading(false);
     };
     fetchPhotos();
-  }, [props.searchValue]);
+  }, [props.searchValue, currentPage]);
+
+  let allPages = [];
+  for (let i = 1; i <= totalPages; i++) {
+    allPages.push(i);
+  }
+
+  console.log(allPages);
 
   if (!isLoading && photos.length === 0) {
     return (
@@ -45,6 +55,19 @@ const Photos = (props) => {
         {photos.map((photo) => {
           return <Photo key={photo.id} photo={photo} />;
         })}
+      </div>
+      <div className={classes["buttons-container"]}>
+        {allPages > 1 &&
+          allPages.map((page) => {
+            return (
+              <button
+                className={classes.button}
+                onClick={() => setCurrentPage(page)}
+              >
+                {page}
+              </button>
+            );
+          })}
       </div>
       {photos.length > 9 && (
         <a className={classes.btn} href="#top" variant="contained">
